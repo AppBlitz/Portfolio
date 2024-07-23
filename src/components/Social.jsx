8c3be0343a3666ad1f4d9a33151484c4baab91ad
carlos <karlcorralesmovil@gmail.com>
2024-07-22 20:17:23 -0500

    components

---
+++ src/components/Social.jsx
@@ -0,0 +1,14 @@
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+function Social() {
+  return (
+    <>
+      <a href="https://github.com/AppBlitz">
+        <AiFillGithub />
+      </a>
+      <a href="https://www.linkedin.com/in/carlos-fabi%C3%A1n-corrales-zapata-48805731b/">
+        <AiFillLinkedin />
+      </a>
+    </>
+  );
+}
+export default Social;
